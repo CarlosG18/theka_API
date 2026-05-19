1c7c9903e2a88de9d37f33e82abbb867cb38288e
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2026-05-19 20:00:25 -0300

    Revert "testando link reset"

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import (
-    LegacyPasswordResetConfirmRedirectView,
-    UserViewSet,
-    PasswordResetConfirmView,
-    PasswordResetView,
-)
+from .views import UserViewSet, PasswordResetConfirmView, PasswordResetView
 
 router = DefaultRouter()
 
@@ -13,11 +8,6 @@ router.register('users', UserViewSet, basename='user')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path(
-        'password-reset-confirm/<str:uid>/<str:token>/',
-        LegacyPasswordResetConfirmRedirectView.as_view(),
-        name='legacy_password_reset_confirm',
-    ),
     path('auth/password/reset/', PasswordResetView.as_view(), name='password_reset'),
     path('auth/password/reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
